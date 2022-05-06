db8568053474559d189c3178a2c9ae55dfd79b67
guilhermelima18 <guilhermelima18@hotmail.com>
2022-05-05 23:26:18 -0300

    fix: update files git

---
+++ server/src/repositories/prisma/prisma-feedbacks.ts
@@ -0,0 +1,14 @@
+import { prisma } from "../../prisma";
+import { FeedbackCreateData, FeedbacksRepository } from "../feedbacks";
+
+export class PrismaFeedbacksRepository implements FeedbacksRepository {
+  async create(data: FeedbackCreateData) {
+    await prisma.feedback.create({
+      data: {
+        type: data.type,
+        comment: data.comment,
+        screenshot: data.screenshot,
+      },
+    });
+  }
+}

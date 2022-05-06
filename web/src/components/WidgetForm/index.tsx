73a48db8b7a17a0d7663bb520d19aea00bad9e3e
guilhermelima18 <guilhermelima18@hotmail.com>
2022-05-05 21:23:21 -0300

    feat: finalizando o front-end web

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
-import { CloseButton } from "../CloseButton";
 import bugImageUrl from "../../assets/bug.svg";
 import ideaImageUrl from "../../assets/idea.svg";
 import otherImageUrl from "../../assets/other.svg";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSuccesStep } from "./Steps/FeedbackSuccessStep";
 
-const feedbackTypes = {
+export const feedbackTypes = {
   BUG: {
     title: "Problema",
     image: {
@@ -28,34 +30,35 @@ const feedbackTypes = {
   },
 };
 
-type FeedbackType = keyof typeof feedbackTypes;
+export type FeedbackType = keyof typeof feedbackTypes;
 
 export const WidgetForm = () => {
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  const [feedbackSent, setFeedbackSent] = useState(false);
+
+  function handleRestartFeedback() {
+    setFeedbackSent(false);
+    setFeedbackType(null);
+  }
 
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      <header>
-        <span className="text-xl leading-6">Deixe seu feedback</span>
-
-        <CloseButton />
-      </header>
-
-      {!feedbackType ? (
-        <div className="flex py-8 gap-2 w-full">
-          {Object.entries(feedbackTypes).map(([key, value]) => (
-            <button
-              key={key}
-              className="bg-zinc-800 rounded-lg py-4 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-              onClick={() => setFeedbackType(key as FeedbackType)}
-            >
-              <img src={value.image.source} alt={value.image.alt} />
-              <span>{value.title}</span>
-            </button>
-          ))}
-        </div>
+      {feedbackSent ? (
+        <FeedbackSuccesStep
+          onFeedbackRestartRequested={handleRestartFeedback}
+        />
       ) : (
-        <p>Hello World</p>
+        <>
+          {!feedbackType ? (
+            <FeedbackTypeStep setFeedbackType={setFeedbackType} />
+          ) : (
+            <FeedbackContentStep
+              feedbackType={feedbackType}
+              onFeedbackRestartRequested={handleRestartFeedback}
+              onFeedbackSent={setFeedbackSent}
+            />
+          )}
+        </>
       )}
 
       <footer className="text-xs text-neutral-400">
